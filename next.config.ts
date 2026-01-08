4da61667d71e4d0ce62cde7f7112a1c2728e65c1
Earnwithalee7890 <earnwithalee@gmail.com>
2026-01-09 00:02:46 +0500

    Configure 307 redirect for Farcaster Hosted Manifest

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019b9efc-9735-0080-af0b-3f04862178ca',
+        permanent: false, // 307 Temporary Redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
