01297b602ff6cc3e49bd97285db146892fbc882d
Earnwithalee7890 <earnwithalee@gmail.com>
2026-01-14 17:11:24 +0500

    refactor: add barrel export for lib utilities

---
+++ src/lib/index.ts
@@ -0,0 +1,10 @@
+// Export all lib utilities for easy imports
+
+export * from './constants';
+export * from './format';
+export * from './validation';
+export * from './performance';
+export * from './keyboard';
+export * from './storage';
+export * from './errors';
+export * from './analytics';
